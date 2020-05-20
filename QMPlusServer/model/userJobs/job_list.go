3852385546f1db93bdc058bbccaedf074a003197
范洪月 <fanhongyue@zhongan.io>
2020-05-20 17:42:25 +0800

    asd

--- QMPlusServer/model/userJobs/job_list.go
+++ QMPlusServer/model/userJobs/job_list.go
@@ -153,7 +153,7 @@ func (jl *Joblist) GetInfoListSearchDetail(keyword string, cityId, order int, in
 
 	outData := time.Now().Unix()
 
-	db := qmsql.DEFAULTDB.Model(jl).Where("p_status != 3 and p_outdate >= ?", outData)
+	db := qmsql.DEFAULTDB.Model(jl).Where("p_status != 3 and p_outdate >= ? and p_count > 0", outData)
 
 	if cityId > 0 {
 		db = db.Where("job_city_id = ?", cityId)

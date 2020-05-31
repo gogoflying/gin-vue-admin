ad84cfd3fb7a2ca3a648b404812da6f41143ca9a
lirulun <lirulun@126.com>
2020-05-31 15:26:49 +0800

    fix enckey

--- QMPlusServer/controller/servers/encrypt.go
+++ QMPlusServer/controller/servers/encrypt.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 )
 
-var XorKey []byte = []byte{0x0B, 0x0A, 0x8C, 0x0C, 0x93, 0x6D, 0x46, 0x47}
+var EnKey []byte = []byte{0x0B, 0x0A, 0x8C, 0x0C, 0x93, 0x6D, 0x46, 0x47}
 
 func DataEncryPt(eData string) string {
 	var result string
@@ -13,7 +13,7 @@ func DataEncryPt(eData string) string {
 	s := ""
 	bt := []rune(eData)
 	for i := 0; i < len(bt); i++ {
-		s = strconv.FormatInt(int64(byte(bt[i])^XorKey[j]), 16)
+		s = strconv.FormatInt(int64(byte(bt[i])^EnKey[j]), 16)
 		if len(s) == 1 {
 			s = "0" + s
 		}
@@ -31,7 +31,7 @@ func DataDecode(eData string) string {
 	fmt.Println(bt)
 	for i := 0; i < len(eData)/2; i++ {
 		s, _ = strconv.ParseInt(string(bt[i*2:i*2+2]), 16, 0)
-		result = result + string(byte(s)^XorKey[j])
+		result = result + string(byte(s)^EnKey[j])
 		j = (j + 1) % 8
 	}
 	return result

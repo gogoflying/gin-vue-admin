63d061d6bef3ec4870e6291b4a6eaaf077c7cdc4
lirulun <lirulun@126.com>
2020-06-09 22:15:51 +0800

    fix bug send sms

--- QMPlusServer/controller/api/job_status.go
+++ QMPlusServer/controller/api/job_status.go
@@ -65,7 +65,9 @@ func UpdateResumeStatus(c *gin.Context) {
 		servers.ReportFormat(c, false, fmt.Sprintf("更新失败：%v", err), gin.H{})
 	} else {
 		//send sms
-		servers.SendSmsInterview(rs.UserInfo.Mobile, rs.UserInfo.UserName, rs.CompanyName,rs.InterviewLoc)
+		if rs.ResumeStatus == 3 && rs.InterviewTime != "" {
+			servers.SendSmsInterview(rs.UserInfo.Mobile, rs.UserInfo.UserName, rs.CompanyName, rs.InterviewLoc, rs.InterviewTime[0:len(rs.InterviewTime)-3])
+		}
 		servers.ReportFormat(c, true, "更新成功", gin.H{
 			"rers": rers,
 		})

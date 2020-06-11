1120ffec94d760be3083272a9b3b79b3cfa77909
lirulun <lirulun@126.com>
2020-06-11 08:48:37 +0800

    send email

--- QMPlusServer/controller/servers/sendemail.go
+++ QMPlusServer/controller/servers/sendemail.go
@@ -31,10 +31,10 @@ func SendEmail(to, subject, body string) error {
 func UserLeaveSendEmail(to []string, subject, body string) error {
 
 	m := gomail.NewMessage()
-	m.SetHeader("To", to[0])
-	if len(to) == 2 {
-		m.SetAddressHeader("Cc", to[1], "zhaopin") //抄送
+	for _, emailAddr := range to {
+		m.SetHeader("To", emailAddr)
 	}
+
 	m.SetAddressHeader("From", config.GinVueAdminconfig.SendEmail.FromEmail, "salary")
 	// 主题
 	m.SetHeader("Subject", subject)

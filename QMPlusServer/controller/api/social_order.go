d19fd65c1f10019526c2c3aa713640bdd7483de6
lirulun <lirulun@126.com>
2020-09-23 23:36:14 +0800

    test12

--- QMPlusServer/controller/api/social_order.go
+++ QMPlusServer/controller/api/social_order.go
@@ -53,8 +53,8 @@ type WXPayNotifyReq struct {
 }
 
 type WXPayNotifyResp struct {
-	Return_code string `xml:"return_code,attr"`
-	Return_msg  string `xml:"return_msg,attr"`
+	Return_code string `xml:"return_code,innerxml"`
+	Return_msg  string `xml:"return_msg,innerxml"`
 }
 
 type MyMap map[string]interface{}
